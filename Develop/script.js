523ea53822a59540b349966c1324be8e06d2d589
brian-lets-go <brian.w.meenan@gmail.com>
2022-01-23 16:45:28 -0500

    javascrip addition 1

--- Develop/script.js
+++ Develop/script.js
@@ -3,4 +3,13 @@
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 $('#currentDay').html(today);
 
-//
+//listen for click of save button
+$('saveBtn').on("click", function () {
+    
+    var textContent = $(this).siblings(".description").val();
+    var timeId = $(this).parent().attr("id");
+
+    console.log(textContent,timeId);
+    localStorage.setITem(timeId, textContent);
+})
+
